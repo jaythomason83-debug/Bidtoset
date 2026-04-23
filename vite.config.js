49bbbfe28b45b720ff0438eac19db00502ed6a6c
Jay Thomason <jaythomason83@gmail.com>
2026-04-23 13:07:29 -0500

    Fix icon size and PWA name

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'BidToSet - Spades Scorekeeper',
+        short_name: 'BidToSet',
+        theme_color: '#0a0e1b',
+        background_color: '#0a0e1b',
+        display: 'standalone',
+        icons: [
+          { src: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+        ]
+      },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
